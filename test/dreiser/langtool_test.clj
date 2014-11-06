533c3f586f12ad9778485e8a3b19cdb8564627bc
Anton Podviaznikov <podviaznikov@gmail.com>
2014-11-05 17:01:15 -0800

    add more tests

--- test/dreiser/langtool_test.clj
+++ test/dreiser/langtool_test.clj
@@ -24,5 +24,16 @@
       (is (= (count (:suggestedReplacements (first checks))) 9))
       (is (= (:offset (first checks)) 0))
       (is (= (:toPos (first checks)) 4))
-      (is (= (:fromPos (first checks)) 0)))))
+      (is (= (:fromPos (first checks)) 0))))
+  (testing "Find one problem with repetitive the"
+    (let [result (ltool/check "The morning is good. The day is good. The night is good." "BritishEnglish")
+          checks (:checks result)]
+      (is (= (:value result) "The morning is good. The day is good. The night is good."))
+      (is (= (count checks) 1))
+      (is (= (:shortMessage (first checks)) "Three successive sentences begin with the same word."))
+      (is (= (:message (first checks)) "Three successive sentences begin with the same word. Reword the sentence or use a thesaurus to find a synonym."))
+      (is (= (count (:suggestedReplacements (first checks))) 0))
+      (is (= (:offset (first checks)) 38))
+      (is (= (:toPos (first checks)) 41))
+      (is (= (:fromPos (first checks)) 38)))))
 
